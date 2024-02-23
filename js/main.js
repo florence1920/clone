da63952bed39947970a90b30645681949073334c
lsh <jetaime12121@gmail.com>
2024-02-23 17:44:38 +0900

    완성

--- js/main.js
+++ js/main.js
@@ -1,30 +1,14 @@
-//글자에 wave 애니메이션
-document.addEventListener("DOMContentLoaded", function() {
-  const waveText = document.querySelector(".header__motto--wave");
-  const letters = waveText.querySelectorAll(".letter");
-
-  // 각 글자마다 랜덤한 애니메이션 지연과 속도
-  letters.forEach(function(letter) {
-    letter.style.animationDelay = Math.random() * 0.5 + "s";
-    letter.style.animationDuration = Math.random() * 1 + 1 + "s";
-  });
-});
-
-// 메일 이미지 요소흔들기
-const mailImg = document.querySelector('.header__mail--shake');
-
-mailImg.addEventListener('click', function() {
-  this.style.transform = 'rotate(-15deg)';
-  setTimeout(() => {
-    this.style.transform = 'rotate(0deg)'; 
-  }, 500); 
-});
-
 //로고 따라 다니기
 const headerLogo = document.querySelector('.header__logo');
 const logoImage = headerLogo.querySelector('img');
 const originalSrc = logoImage.src;
-const newSrc = '../images/logo_on.png';
+const currentURL = window.location.href;
+let newSrc = '';
+if(currentURL === 'http://127.0.0.1:5500/pages/index.html'){
+  newSrc = '../images/logo_on.png'
+}else if(currentURL === 'http://127.0.0.1:5500/pages/about.html'){
+  newSrc = '../images/logo-white_on.png'
+}
 
 headerLogo.addEventListener('mouseenter', () => {
   logoImage.src = newSrc;
@@ -59,33 +43,39 @@ function moveImage() {
 
 function resetImage() {
   logoImage.src = originalSrc;
-  logoImage.style.transition = 'left 1s, top 1s'; // transition 추가
+  logoImage.style.transition = 'left 1s, top 1s'; 
   logoImage.style.left = '50%';
   logoImage.style.top = '50%';
-  // transition이 적용된 후에 transition 속성을 초기화하여 다음에 마우스가 다시 a 태그 영역 안에 들어왔을 때 다시 적용되지 않도록 합니다.
+  
   setTimeout(() => {
     logoImage.style.transition = 'none';
-  }, 1000); // transition 시간(1초)이 지난 후에 transition을 초기화합니다.
+  }, 1000); 
 }
 
-//curious 마우스 올렸을 때 효과
-// 마우스를 올렸을 때 효과를 활성화하는 함수
 
-let isEffectActive = false;
-function activateEffect() {
-  if(!isEffectActive){
-    isEffectActive = true;
-    const overlay = document.querySelector('.fullscreen--overlay');
-    overlay.style.display = 'block';
-    // 3초 후에 효과를 비활성화
-    setTimeout(() => {
-      overlay.style.display = 'none';
-      isEffectActive = false;
-    }, 2000); 
-  }
-}
+//글자에 wave 애니메이션
+document.addEventListener("DOMContentLoaded", function() {
+  const waveText = document.querySelector(".header__motto--wave");
+  const letters = waveText.querySelectorAll(".letter");
 
-document.querySelector('.header__motto--wave').addEventListener('mouseover', activateEffect);
+// 각 글자마다 랜덤한 애니메이션 지연과 속도
+letters.forEach(function(letter) {
+    letter.style.animationDelay = Math.random() * 0.5 + "s";
+    letter.style.animationDuration = Math.random() * 1 + 1 + "s";
+  });
+});
+
+
+// 메일 이미지 요소흔들기
+const mailImg = document.querySelector('.header__mail--shake');
+
+mailImg.addEventListener('click', function() {
+  this.style.transform = 'rotate(-15deg)';
+  setTimeout(() => {
+    this.style.transform = 'rotate(0deg)'; 
+  }, 500); 
+});
+
 
 //모달 나오는 함수 
 document.querySelectorAll('.modal__wrap').forEach( wrap => {
@@ -98,36 +88,6 @@ document.querySelectorAll('.modal__wrap').forEach( wrap => {
   })
 })
 
-
-//grid li 에 hover 했을 때 
-document.querySelectorAll('.article__contents--grid > li').forEach( li => {
-  li.addEventListener('mouseenter', function() {
-    this.querySelector('.contents__cover').style.opacity = 1;
-  })
-  li.addEventListener('mouseleave', function() {
-    this.querySelector('.contents__cover').style.opacity = 0;
-  })
-})
-
-//display grid, flex 교체
-const flexEl = document.querySelector('.article__contents--flex');
-const gridEl = document.querySelector('.article__contents--grid');
-const viewIcon = document.querySelector('.article--view');
-let isGrid = false;
-
-document.querySelector('.article--view').addEventListener('click',()=>{
-  if(!isGrid){
-    gridEl.style.display = 'grid';
-    flexEl.style.display = 'none';
-    viewIcon.innerHTML = 'view_list';
-    isGrid =! isGrid
-  }else if(isGrid){
-    gridEl.style.display = 'none';
-    flexEl.style.display = 'block';
-    viewIcon.innerHTML = 'grid_view';
-    isGrid =! isGrid
-  }
-})
 
 // 스크롤 할 때 요소 skew
 let isScrolling = false;
@@ -198,34 +158,3 @@ cursorScale.forEach(link => {
     cursor.classList.remove('grow-small');
   });
 })
-
-//li 마우스 hover 이미지 그 자리에 등장
-let imgHoverTimer; 
-
-const liElements = document.querySelectorAll('.article__contents--flex > li');
-
-liElements.forEach(li => {
-  const imgHover = li.querySelector('.img--hover'); 
-
-  // .img--hover 요소가 있는 경우에만 이벤트 리스너 등록
-  if (imgHover) {
-    li.addEventListener('mouseenter', function(event) {
-      imgHover.style.display = 'block';
-      imgHover.style.left = event.clientX - this.getBoundingClientRect().left + 'px';
-      imgHover.style.top = event.clientY - this.getBoundingClientRect().top + -100 + 'px';
-
-      clearTimeout(imgHoverTimer);
-
-      //이미지 숨기기
-      imgHoverTimer = setTimeout(() => {
-        imgHover.style.display = 'none';
-      }, 1000);
-    });
-
-    li.addEventListener('mouseleave', function(event) {
-      imgHover.style.display = 'none';
-
-      clearTimeout(imgHoverTimer);
-    });
-  }
-});
